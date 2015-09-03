b0ff95b185ca59a9e4aefff22442b34e7b45d061
vasu <vanka.vasu@gmail.com>
2015-09-03 13:48:25 +0530

    reportsocketcalls-reportModule Update

---
+++ controller/reportSocketCalls.js
@@ -0,0 +1,19 @@
+
+function reportSocketCalls(report,db,socket){
+	socket.on('buySharesFromOrg',function(data){
+		console.log('in buySharesFromOrg socket call')
+		report.buySharesFromOrg(data,function(result){
+			socket.emit('buySharesFromOrg',result);
+		})
+	})
+	socket.on('sellSharesFromOrg',function(data){
+		console.log('in sellSharesFromOrg socket call')
+		report.sellSharesFromOrg(data,function(result){
+			socket.emit('sellSharesFromOrg',result);
+		})
+	})
+
+
+	
+}
+module.exports.reportSocketCalls = reportSocketCalls;
