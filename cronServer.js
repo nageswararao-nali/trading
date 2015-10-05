974ceaaf73e6b543fe2d8f9f86b597c5ef28b5de
Nageswararaonali <nnageswararao.nali@gmail.com>
2015-10-05 13:46:13 +0530

    updates

--- cronServer.js
+++ cronServer.js
@@ -34,15 +34,62 @@ var headers= {
            	"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11",
            	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*;q=0.8",
            }
+
 function getUrl(urlCallback){
 	var url = "http://www.nseindia.com/products/dynaContent/equities/equities/htms/CM_homepage.htm";
 	request.get({uri:url,headers:headers},function(error,response,content){
 		if(!error && response.statusCode == 200){
 			// console.log(content)
 			$ = cheerio.load(content);
-			console.log($(".report").eq(1).find("ul").eq(2).find("li a").attr("href"))
-			var fileUrl = "http://www.nseindia.com/"+$(".report").eq(1).find("ul").eq(2).find("li a").attr("href");
-			var fileName = fileUrl.split("/").pop();
+			var i=1;
+			function findLoop(i){
+				var fileUrl = "http://www.nseindia.com/"+$(".report").eq(1).find("ul").eq(i).find("li a").attr("href");
+				var fileName = fileUrl.split("/").pop();
+				var fileNameExt = fileName.split(".")[1];
+				if(fileNameExt === "csv"){
+					console.log(i)
+					console.log($(".report").eq(1).find("ul").eq(i).find("li a").attr("href"))
+					urlCallback(fileUrl)
+				}else{
+					i++;
+					findLoop(i);
+				}
+			}findLoop(i)
+		}else{
+			console.log("error in request")
+			console.log(response)
+		}
+	})
+}
+function getFutureCMP(dZipFileName,callback){
+	console.log("************************************")
+	var monthNames = { "JAN":"01", "FEB":"02", "MAR":"03", "APRL":"04", "MAY":"05", "JUN":"06", "JUL":"07", "AUG":"08", "SEP":"09", "OCT":"10", "NOV":"11", "DEC":"12" };
+	var day = dZipFileName.substr(0,4).substr(2,2);
+	var month = dZipFileName.substring(4,dZipFileName.indexOf(dZipFileName.substr(-16)));
+	var year = dZipFileName.substr(-16).substr(0,4);
+	month = monthNames[month]
+	var fdate = day+"-"+month+"-"+year;
+	console.log(fdate);
+	console.log("************************************")
+	var futureUrl = "http://www.nseindia.com/ArchieveSearch?h_filetype=fosett&date="+fdate+"&section=FO";
+	//var url = "http://www.nseindia.com/products/dynaContent/equities/equities/htms/CM_homepage.htm";
+	request.get({uri:futureUrl,headers:headers},function(error,response,content){
+		if(!error && response.statusCode == 200){
+			// console.log(content)
+			$ = cheerio.load(content);
+			console.log("future url issssssssssssssss")
+			console.log($("a").attr("href"))
+			var fileUrl = "http://www.nseindia.com/"+$("a").attr("href");
+			var dZipFileName = fileUrl.split("/").pop();
+			var dPath = "assets/dailyfutures/"+dZipFileName;
+			request.get({uri:fileUrl,headers:headers}).pipe(fs.createWriteStream(dPath))
+			setTimeout(function(){
+				parseFutureCsvFile('assets/dailyfutures/'+dZipFileName,function(data){
+					callback(data)
+				})
+				// test1('assets/dailyfutures/'+dZipFileName)
+			},10000)
+			/*var fileName = fileUrl.split("/").pop();
 			var fileNameExt = fileName.split(".")[1];
 			if(fileNameExt === "csv"){
 				console.log("in first")
@@ -51,30 +98,98 @@ function getUrl(urlCallback){
 				console.log("in second")
 				var fileUrl = "http://www.nseindia.com/"+$(".report").eq(1).find("ul").eq(3).find("li a").attr("href");
 				urlCallback(fileUrl)
-			}
+			}*/
 		}else{
 			console.log("error in request")
-			console.log(response)
+			// console.log(response)
+			callback([])
 		}
 	})
 }
+function parseFutureCsvFile(inputFile,callback){
+	// var inputFile='assets/ofiles/cm03SEP2015bhav.csv';
+	// var inputFile=csvFilePath;
+
+	var companies = [];
+	var header = [];
+	var reportData = [];
+	var parser = parse({delimiter: ','}, function (err, data) {
+		// console.log(err)
+		// console.log(data)
+	  async.eachSeries(data, function (line, callback) {
+	  	console.log("lineeeeeeeeeeeee")
+	  	console.log(line)
+		var dataObj = {};
+	    if(header.length === 0){
+	    	header = line;
+	    	//console.log(header)
+	    	async.eachSeries(header, function (field, headerCallback) {
+	    		if(field !== "")
+	    			dataObj[field] = "";
+	    		headerCallback()
+	    	},function(){
+	    		callback();
+	    	})
+	    }else if(line[3] === ""){
+
+	    	callback()
+	    }else{
+	        header = data[0];
+	        //console.log(header)
+	        async.eachSeries(header, function (field, headerCallback) {
+	            if(field !== "")
+	                dataObj[field] = "";
+	            headerCallback()
+	        },function(){
+	            var i=0,isCom=0;
+	           
+	            // if(isCom){
+                async.eachSeries(line, function (fieldValue, headerValCallback) {
+                    if(fieldValue !== ""){
+                        dataObj[header[i]] = fieldValue;
+                    }
+                    i++;
+                    headerValCallback()
+                },function(){
+                	// dataObj.lastUpdate = new Date();
+                    reportData.push(dataObj)
+                    callback();
+                })
+	            
+	        })
+	    	
+	    }
+	  },function(){
+		console.log(reportData)
+		callback(reportData)
+	  })
+	})
+	// console.log(inputFile)
+	fs.createReadStream(inputFile).pipe(parser);
+}
+
 function getCMP(callback){
 	getUrl(function(dZipFilePath){
 		var dZipFileName = dZipFilePath.split("/").pop();
 		var dPath = "assets/files/"+dZipFileName;
-		request.get({uri:dZipFilePath,headers:headers}).pipe(fs.createWriteStream(dPath))
-		setTimeout(function(){
-			fs.createReadStream(dPath).pipe(unzip.Extract({ path: 'assets/ofiles/' }));
+		getFutureCMP(dZipFileName,function(futuresData){
+			var futuresData = futuresData;
+			request.get({uri:dZipFilePath,headers:headers}).pipe(fs.createWriteStream(dPath))
 			setTimeout(function(){
-				parseCsvFile('assets/ofiles/'+dZipFileName);
+				fs.createReadStream(dPath).pipe(unzip.Extract({ path: 'assets/ofiles/' }));
 				setTimeout(function(){
-					callback()
-				},20000)
-			},2000)
-		},5000)
+					parseCsvFile('assets/ofiles/'+dZipFileName,futuresData);
+					// callback function to run closing balance caliculations
+					// setTimeout(function(){
+					// 	callback()
+					// },20000)
+				},2000)
+			},5000)
+		});
+		
 	})
 }
-function parseCsvFile(csvFilePath){
+function parseCsvFile(csvFilePath,futuresData){
 	// var inputFile='assets/ofiles/cm03SEP2015bhav.csv';
 	var filepath = csvFilePath.split(".");
 	filepath.pop();
@@ -150,6 +265,7 @@ function parseCsvFile(csvFilePath){
 	    })*/
 		var cListData = {};
 		cListData.cList = reportData;
+		cListData.fList = futuresData;
 		cListData.createDate = new Date();
 		var cListDataQuery = new db.CompanyList(cListData) 
 		cListDataQuery.save(function(err,list){
@@ -182,56 +298,25 @@ function getSharesValue(pName,date,callback){
 			function rLoop(j){
 				var report = reports[j];
 				console.log('cName '+report.cName)
-				var cquery = "";
-
-				if(report.segment === "Equity"){
-					cquery = {'$where': 'this.createDate.toJSON().slice(0, 10) == "'+dateString+'"',"cList.SYMBOL":report.cName}
-				}else if(report.segment === "Futures"){
-					// console.log('fmonth '+report.fMonth);
-					// var months = report.fMonth.split('-')
-					// console.log("months "+months+' month name '+months[1])
-					cquery = {'$where': 'this.createDate.toJSON().slice(0, 10) == "'+dateString+'"',"fList.UNDERLYING":report.cName}
-
-				}
-				db.CompanyList.findOne(cquery,function(err,companyDetails){
-					console.log('companyDetails '+companyDetails.createDate)
+				db.CompanyList.findOne({'$where': 'this.createDate.toJSON().slice(0, 10) == "'+dateString+'"',"cList.SYMBOL":report.cName},function(err,companyDetails){
+					//console.log('companyDetails '+companyDetails.length)
 					if(!err && companyDetails){
 						var rp = {};
-						cmp = 0
 						// rp.cName = report.cName;
 						// rp.pName = report.pName;
 						// rp.quantity = report.quantity;
 						// rp.price = report.price;
 						// rp.total = report.total;
 						// rp.rMark = report.releaseMark;
-						//console.log('companyDetails length '+companyDetails.cList.length)
-						if(report.segment === "Equity"){
-							for(var i = 0 ; i<companyDetails.cList.length ; i++){
-								if(companyDetails.cList[i].SYMBOL === report.cName){
-									if(companyDetails.cList[i].CLOSE !== undefined)
-										cmp = companyDetails.cList[i].CLOSE;
-									else
-										cmp = 0;	
-								}
-								//console.log('companyDetails '+companyDetails.cList[i].SYMBOL)
+						console.log('companyDetails length '+companyDetails.cList.length)
+						for(var i = 0 ; i<companyDetails.cList.length ; i++){
+							if(companyDetails.cList[i].SYMBOL === report.cName){
+								if(companyDetails.cList[i].CLOSE !== undefined)
+									cmp = companyDetails.cList[i].CLOSE;
+								else
+									cmp = 0;	
 							}
-						}else if(report.segment === "Futures"){
-							console.log('details in Futures ')
-							for(var i = 0 ; i<companyDetails.fList.length ; i++){
-								var exp = "EXPIRY DATE"
-								var da = companyDetails.fList[i][exp].split('-')								
-								if(da[1] === report.fMonth){
-									//console.log('contract months  '+da[1])
-									if(companyDetails.fList[i].UNDERLYING === report.cName){
-										var s = "MTM SETTLEMENT PRICE"
-										if(companyDetails.fList[i][s] !== undefined)
-											cmp = companyDetails.fList[i][s];
-										else
-											cmp = 0;	
-									}
-								}
-								//console.log('companyDetails '+companyDetails.cList[i].SYMBOL)
-							}
+							//console.log('companyDetails '+companyDetails.cList[i].SYMBOL)
 						}
 						console.log('cmp '+cmp+' cName '+report.cName+' and quantity '+report.quantity)
 
@@ -273,53 +358,53 @@ function getSharesValue(pName,date,callback){
 	})
 }
 
-function updateToBalances(pName,date,closingBalance,callback){
-	//var cDate = new Date()
-	var dateString = date.toJSON().slice(0, 10)
+	function updateToBalances(pName,date,closingBalance,callback){
+		//var cDate = new Date()
+		var dateString = date.toJSON().slice(0, 10)
 
-	db.portFolioBalances.find({pName : pName,'$where': 'this.date.toJSON().slice(0, 10) == "'+dateString+'"' },function(err,result){
-		if(!err && result.length>0){
-			db.portFolioBalances.update({_id:result[0]._id},{closeBal : closingBalance },function(err,updated){
-				if(!err && updated){
-					//console.log('updated success')
-					callback()
-				}
-			})
-		}else{
-			db.portFolioBalances.find({pName : pName}).sort({date:-1}).limit(1).exec(function(err,docs){
-				if(!err && docs.length>0){
-					console.log('closeBal '+docs[0].closeBal)
-					new db.portFolioBalances({pName : pName , openBal : docs[0].closeBal, closeBal : docs[0].closeBal, date: date}).save(function(err,inserted){
-						if(!err && inserted){
-							db.portFolioBalances.update({_id:inserted._id},{closeBal : closingBalance },function(err,updated){
-								if(!err && updated){
-									//console.log('updated success')
-									callback()
-								}
-							})
-						}
-					})						
-				}else{
-					db.portFolio.find({pName : pName },function(err,amountInfo){
-						if(!err && amountInfo){
-							new db.portFolioBalances({pName : pName , openBal : amountInfo[0].capital, closeBal : amountInfo[0].capital, date: date}).save(function(err,inserted){
-								if(!err && inserted){
-									db.portFolioBalances.update({_id:inserted._id},{closeBal : closingBalance },function(err,updated){
-										if(!err && updated){
-											//console.log('updated success')
-											callback()
-										}
-									})
-									//callback()
-								}
-							})
-						}
-					})
-				}
-			})
-		}	
-	})
-}
+		db.portFolioBalances.find({pName : pName,'$where': 'this.date.toJSON().slice(0, 10) == "'+dateString+'"' },function(err,result){
+			if(!err && result.length>0){
+				db.portFolioBalances.update({_id:result[0]._id},{closeBal : closingBalance },function(err,updated){
+					if(!err && updated){
+						//console.log('updated success')
+						callback()
+					}
+				})
+			}else{
+				db.portFolioBalances.find({pName : pName}).sort({date:-1}).limit(1).exec(function(err,docs){
+					if(!err && docs.length>0){
+						console.log('closeBal '+docs[0].closeBal)
+						new db.portFolioBalances({pName : pName , openBal : docs[0].closeBal, closeBal : docs[0].closeBal, date: date}).save(function(err,inserted){
+							if(!err && inserted){
+								db.portFolioBalances.update({_id:inserted._id},{closeBal : closingBalance },function(err,updated){
+									if(!err && updated){
+										//console.log('updated success')
+										callback()
+									}
+								})
+							}
+						})						
+					}else{
+						db.portFolio.find({pName : pName },function(err,amountInfo){
+							if(!err && amountInfo){
+								new db.portFolioBalances({pName : pName , openBal : amountInfo[0].capital, closeBal : amountInfo[0].capital, date: date}).save(function(err,inserted){
+									if(!err && inserted){
+										db.portFolioBalances.update({_id:inserted._id},{closeBal : closingBalance },function(err,updated){
+											if(!err && updated){
+												//console.log('updated success')
+												callback()
+											}
+										})
+										//callback()
+									}
+								})
+							}
+						})
+					}
+				})
+			}	
+		})
+	}
 
 function updateClosingBalance(pName,date,maincallback){
 	//process : every day evening execute this function
@@ -352,13 +437,13 @@ function updateClosingBalance(pName,date,maincallback){
 	)
 }
 
-// var yesterday = new Date(new Date() - 24*60*60*1000)
-// console.log('yesterday '+yesterday)
+var yesterday = new Date(new Date() - 24*60*60*1000*11)
+console.log('yesterday '+yesterday)
 
-// updateClosingBalance("Nicobar Capital",yesterday,function(closingBalance,msg){
+// updateClosingBalance("Nicobar Capital",new Date(),function(closingBalance,msg){
 // 	console.log('status of updateClosingBalance is '+closingBalance+' message '+msg)
 // })
-//runScript();
+runScript();
 setInterval(function(){
   runScript()
   
